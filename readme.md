2b96b7dd2b338b6554ea7b1f9f6eab93b61767d7
greatmaxix <b.maksat@dogovor24.kz>
2021-03-23 19:16:49 +0600

    done

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # this repo is for nestjs course projects
+
+nest g module *module paths* - create module
+nest g controller *controller path* --no-spec - create controller
+nest g service tasks --no-spec
---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from 'class-validator'
+
+export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    username: string;
+    
+    
+    @IsString()
+    @MinLength(8)
+    @Matches(
+        /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+        { message: 'Password is too weak' }
+    )
+    password: string;
+}
